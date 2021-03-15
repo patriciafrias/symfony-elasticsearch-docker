a2a21a9f8c0adadea0b03635af39145e6b8ed92a
Patricia Frias <patriciafm4@gmail.com>
2021-03-15 16:57:36 +0100

    wip: first steps to implement graphql

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Overblog\GraphiQLBundle\OverblogGraphiQLBundle::class => ['dev' => true],
+    Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
 ];
